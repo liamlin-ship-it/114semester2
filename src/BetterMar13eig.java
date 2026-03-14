b6d6078f18c800ff0b9ae28e1df872b9b721f041
LiamLin <liamlin9606@gmail.com>
2026-03-14 12:39:01 +0800

    Add BetterMar13eig

---
+++ src/BetterMar13eig.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BetterMar13eig {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        boolean isPrime = true;
+        if (n <= 1){
+            isPrime = false;
+        }else {
+            for (int i = 2; i * i <= n; i++){
+                if (n % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(isPrime);
+        scanner.close();
+    }
+}
